c40e1495870dba4e29e186252f052ad44c02eee2
Moonshot <moonshot11@users.noreply.github.com>
2024-01-26 18:09:44 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+
+namespace ShadesOfFriends
+{
+    /// <summary>
+    /// Possible genders in SoD citizen data
+    /// </summary>
+    public enum Gender
+    {
+        Male,
+        Female,
+        Any,
+        Nonbinary
+    }
+
+    /// <summary>
+    /// A custom entry provided by the user
+    /// </summary>
+    public struct Person
+    {
+        string name;
+        
+    }
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            string user = System.Environment.UserName;
+
+            OpenFileDialog cityOFD = new OpenFileDialog();
+            cityOFD.Title = "Select city file.";
+            cityOFD.InitialDirectory = @$"C:\Users\{user}\AppData\LocalLow\ColePowered Games\Shadows of Doubt\Cities\";
+            cityOFD.Filter = "City files|*.cit;*.citb";
+            cityOFD.ShowDialog();
+
+            OpenFileDialog namesOFD = new OpenFileDialog();
+            cityOFD.Title = "Select custom names text file.";
+            cityOFD.InitialDirectory = @$"C:\Users\{user}\AppData\LocalLow\ColePowered Games\Shadows of Doubt\Cities\";
+            cityOFD.Filter = "Text files|*.txt";
+            cityOFD.ShowDialog();
+
+            string cityfile = cityOFD.FileName;
+            string namefile = namesOFD.FileName;
+
+            string[] names =
+            {
+                "Alan Alphabet",
+                "Zoey Zebra"
+            };
+            Dictionary<int, string> randmap = new Dictionary<int, string>();
+
+            Console.Write("Reading save file...");
+            string raw = cityfile.EndsWith('b') ?
+                Util.BrotliFileToString(cityfile) :
+                File.ReadAllText(cityfile);
+            Console.WriteLine("done!");
+
+            Console.Write("Parsing data...");
+            JObject obj = JObject.Parse(raw);
+            Console.WriteLine("done!");
+
+            JToken[] people = obj["citizens"].ToArray();
+            Random rand = new Random();
+
+            // Pick random citizens to replace
+            while (randmap.Count < names.Length)
+            {
+                int sel = rand.Next() % people.Length;
+                if (!randmap.ContainsKey(sel))
+                    randmap.Add(sel, names[randmap.Count]);
+            }
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadesOfFriends
+{
+    public static class Util
+    {
+        /// <summary>
+        /// Takes the name of a Brotli bin file; returns a decompressed ASCII string
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string BrotliFileToString(string filename)
+        {
+            byte[] buf;
+
+            using (FileStream fs = new(filename, FileMode.Open, FileAccess.Read))
+            using (BrotliStream bs = new(fs, CompressionMode.Decompress))
+            using (MemoryStream ms = new())
+            {
+                bs.CopyTo(ms);
+                buf = ms.ToArray();
+            }
+            return Encoding.ASCII.GetString(buf);
+        }
+
+        /// <summary>
+        /// Write the contents of a string to a file
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="filename"></param>
+        public static void StringToBrotliFire(string data, string filename)
+        {
+            using (MemoryStream ms = new())
+            using (BrotliStream bs = new(ms, CompressionMode.Compress))
+            using (FileStream fs = new(filename, FileMode.CreateNew, FileAccess.Write))
+            {
+                byte[] buf = Encoding.ASCII.GetBytes(data);
+                ReadOnlySpan<byte> span = new(buf);
+                ms.Write(span);
+                bs.CopyTo(fs);
+            }
+        }
+    }
+}
