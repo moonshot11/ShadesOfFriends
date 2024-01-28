fa70bd74a18063d3a57cf96a4410ce9a0e300782
Moonshot <moonshot11@users.noreply.github.com>
2024-01-28 07:40:59 -0800

    Cleanup and some simple error checking

--- Program.cs
+++ Program.cs
@@ -20,6 +20,8 @@ namespace ShadesOfFriends
         Male = 0,
         Female = 1,
         Nonbinary = 2,
+
+        // Patcher-specific mode, doesn't appear in game
         Any = 3
     }
 
@@ -36,37 +38,37 @@ namespace ShadesOfFriends
         public readonly string Fullname =>
             string.Join(' ', new string[] { First, Last }.Where(x => x != null));
     }
+
     public class Program
     {
         [STAThread]
-        public static void Main()
+        public static int Main()
         {
             string user = Environment.UserName;
             string citiesPath = @$"C:\Users\{user}\AppData\LocalLow\ColePowered Games\Shadows of Doubt\Cities\";
 
             Console.Write("Prompting for city file: ");
             OpenFileDialog cityOFD = new();
+            OpenFileDialog namesOFD = new();
+
+            Console.Write("Prompting for city file: ");
             cityOFD.Title = "Select city file.";
             cityOFD.InitialDirectory = citiesPath;
             cityOFD.Filter = "City files|*.cit;*.citb";
             if (cityOFD.ShowDialog() != DialogResult.OK)
-                return;
+                return 0;
             Console.WriteLine(Path.GetFileName(cityOFD.FileName));
 
             Console.Write("Prompting for custom names file: ");
-            OpenFileDialog namesOFD = new();
             namesOFD.Title = "Select custom names text file.";
             namesOFD.InitialDirectory = Environment.CurrentDirectory;
             namesOFD.Filter = "Text files|*.txt";
             if (namesOFD.ShowDialog() != DialogResult.OK)
-                return;
+                return 0;
             Console.WriteLine(Path.GetFileName(namesOFD.FileName));
 
             string cityFilename = cityOFD.FileName;
             string namesFilename = namesOFD.FileName;
-
-            List<Person> arrivals = FetchNameData(namesFilename);
-
             bool enableCompression = cityFilename.EndsWith('b');
             Dictionary<int, Person> randmap = new();
             List< Tuple<string, string> > postWriteNames = new();
@@ -82,6 +84,11 @@ namespace ShadesOfFriends
             JToken[] citizens = obj["citizens"].ToArray();
             Random rand = new();
 
+            Console.WriteLine("Reading custom names");
+            List<Person> arrivals = FetchNameData(namesFilename);
+
+            // -- Randomly choose which citizens' names to replace --
+
             Console.WriteLine("Choosing citizens to replace");
             // Pick random citizens to replace
             while (randmap.Count < arrivals.Count)
@@ -95,8 +102,9 @@ namespace ShadesOfFriends
                     randmap.Add(sel, arrival);
             }
 
+            // -- Substitute citizen names --
+
             Console.WriteLine("Replacing citizens");
-            // Report and implement primary mapping
             foreach (var kv in randmap)
             {
                 Person arrival = kv.Value;
@@ -118,17 +126,27 @@ namespace ShadesOfFriends
                 Console.WriteLine("  " + origname + " -> " + arrival.Fullname);
             }
 
+            // -- Clean up dangling mentions of original names --
+
             string cityOutput = JsonConvert.SerializeObject(obj, Formatting.None);
 
             Console.WriteLine("Cleaning stale citizen data");
             foreach ((string oldname, string newname) in postWriteNames)
                 cityOutput = cityOutput.Replace(oldname, newname);
 
-            // Write updated city data to new file
+            // -- Write new data --
+
+            // Back up original city file
             string backupFilename = Util.FindValidBackupName(cityFilename);
             Console.WriteLine("Creating backup of original data: " + Path.GetFileName(backupFilename));
             File.Move(cityFilename, backupFilename);
+            if (!File.Exists(backupFilename))
+            {
+                Console.WriteLine("ERROR: Could not back up original file. Aborting.");
+                return 1;
+            }
 
+            // Create new city file with updated names
             Console.WriteLine("Writing patched file to Cities folder");
             if (enableCompression)
             {
@@ -140,10 +158,23 @@ namespace ShadesOfFriends
                 File.WriteAllText(cityFilename, cityOutput);
             }
 
+            if (!File.Exists(cityFilename))
+            {
+                Console.WriteLine("ERROR: Could not write patched city file. Aborting.");
+                return 1;
+            }
+
             Console.WriteLine("Done! Press any key to exit.");
             Console.ReadKey(true);
+            return 0;
         }
 
+        /// <summary>
+        /// Parse the user-provided listfile of custom names
+        /// and return a populated list of Person objects.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
         public static List<Person> FetchNameData(string filename)
         {
             string[] lines = File.ReadAllLines(filename);
@@ -157,6 +188,8 @@ namespace ShadesOfFriends
                 if (line == "")
                     continue;
 
+                // If gender directive is found, change current mode
+                // and skip to next line
                 switch (line)
                 {
                     case "#any":
@@ -175,25 +208,25 @@ namespace ShadesOfFriends
 
                 line = Util.RemoveNonAlpha(line);
                 string[] words = line.Split();
-                Person per = new();
+                Person person = new();
 
                 if (words.Length == 1)
                 {
-                    per.Last = char.ToUpper(words[0][0]) + words[0].Substring(1).ToLower();
+                    person.Last = char.ToUpper(words[0][0]) + words[0][1..].ToLower();
                 }
                 else if (words.Length >= 2)
                 {
-                    per.First = char.ToUpper(words[0][0]) + words[0].Substring(1).ToLower();
-                    per.Last = char.ToUpper(words[1][0]) + words[1].Substring(1).ToLower();
+                    person.First = char.ToUpper(words[0][0]) + words[0][1..].ToLower();
+                    person.Last = char.ToUpper(words[1][0]) + words[1][1..].ToLower();
                 }
 
                 if (words.Length >= 3)
                 {
-                    per.Nick = char.ToUpper(words[2][0]) + words[2].Substring(1).ToLower();
+                    person.Nick = char.ToUpper(words[2][0]) + words[2][1..].ToLower();
                 }
 
-                per.Gender = mode;
-                result.Add(per);
+                person.Gender = mode;
+                result.Add(person);
             }
 
             return result;
